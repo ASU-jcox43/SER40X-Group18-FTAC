dc42e7e27e4a7af3a918d4aac08534a1bc4344ae
Ben Stefl <benstefl8@gmail.com>
2025-10-19 23:18:25 -0700

    US#17 scrapes pdf links from yellowknife's website on the main bylaws page

--- scraper/document_scraper/document_scraper/spiders/DocumentScraper.py
+++ scraper/document_scraper/document_scraper/spiders/DocumentScraper.py
@@ -5,44 +5,24 @@ from scrapy.http.response import Response
 class DocumentscraperSpider(scrapy.Spider):
     name = "DocumentScraper"
     allowed_domains: list[str] = ["yellowknife.ca"]
-    start_urls: list[str] = ["https://www.yellowknife.ca/en/index.aspx"]
-    visited_urls: set[str] = set()
-    pdfs: dict[int, str] = {}
+    start_urls: list[str] = ["https://www.yellowknife.ca/Bylaws/Bylaw?_mid_=34596"]
     pdf_count: int = 0
-    next_urls: list[str] = []
 
     def parse(self, response: Response):
-        # Use BFS to traverse the entire website of municipality X
+       #check if any urls are PDFs by calling check() on them
+        for url in response.xpath("//@href").getall():
+            if self.allowed_domains[0] in url or url.startswith('/'):
+                print(f'following {url}')
+                yield response.follow(url, callback=self.check)
 
-        # Mark the current URL as visited
-        self.visited_urls.add(response.url)
-
-        # Store all URLs of PDFs visited in pdfs. You will know when you are visiting a PDF if one of the following conditions are met:
+        #All PDFs will be added by check() to pdfs[] and any other pages will be ignored
+    
+    def check(self, response: Response):
+        # Check the URL and yield it if it is a PDF. You will know when you are visiting a PDF when one of the following conditions are met:
         #   1. the URL of the current page ends with ".pdf"
         #   2. there is "Content-Type: application/pdf" in the response header
-        if response.url.endswith(".pdf") or (
-            "Content-Type" in response.headers
-            and response.headers["Content-Type"] == b"application/pdf"
-        ):
-            self.pdfs[self.pdf_count] = response.url
+        print(f'checking {response.url}: {response.url.endswith(".pdf")}, {response.headers["Content-Type"] if "Content-Type" in response.headers else "no"}')
+        if response.url.endswith(".pdf") or ("Content-Type" in response.headers and response.headers["Content-Type"] == b"application/pdf"):
             self.pdf_count = self.pdf_count + 1
-
-        # append all links on the current page that stay inside of the domain and have not been visited to next_urls
-
-        self.next_urls.extend(
-            list(
-                filter(
-                    lambda s: s not in self.visited_urls
-                    and (self.allowed_domains[0] in s or s.startswith("/")),
-                    response.xpath("//@href").getall(),
-                )
-            )
-        )
-
-        print(f"visit all pages linked to by {response.url}")
-
-        # Visit all pages linked to by the current page
-        if self.next_urls != []:
-            yield response.follow(self.next_urls.pop(0), callback=self.parse)
-        else:
-            yield self.pdfs
+            print(f'about to yield {response.url}')
+            yield {self.pdf_count: response.url}
