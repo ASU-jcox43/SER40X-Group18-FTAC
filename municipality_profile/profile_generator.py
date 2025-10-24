c849dac6407379534e1ada93f9c460ce5498f714
Brandon Kan <brandon.kwan.kan@gmail.com>
2025-10-23 22:53:05 -0700

    updated example value for testing adding profiles

--- municipality_profile/profile_generator.py
+++ municipality_profile/profile_generator.py
@@ -8,6 +8,13 @@ def addProfile(name, province, population, age, income, community):
     # Add logic to ask for profile information
     profile = createProfile(name, province, population, age, income, community)
 
+# example values for testing
+name = "Jacob"
+province = "Ontario"
+population = 2,800,000
+age = 35
+income = 60,0000
+community = "community"
 
 if __name__ == "__main__":
-    addProfile("Jacob", "Toronto", 100, 50, 6000, "community")
+    addProfile(name, province, population, age, income, community)
