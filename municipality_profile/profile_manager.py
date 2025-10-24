eff9f520695a9eefc1fa8721e608ecce76b0723f
Brandon Kan <brandon.kwan.kan@gmail.com>
2025-10-23 22:42:23 -0700

    Created municipality geographic profile section format

--- municipality_profile/profile_manager.py
+++ municipality_profile/profile_manager.py
@@ -16,5 +16,30 @@ def createProfile(name, province, population, age, income, community):
         "Province": province,
         "Demographic": createDemographic(population, age, income, community),
     }
-
     return profile
+
+
+def createGeographic(cityName, province, region, location, landArea, boundaries):
+    # Example geographic profile section  
+    # City Name: City of Makrham
+    # Province: Ontario
+    # Region: York Region (Greater Toronto Area)
+    # Location: East of Toronto
+    # land Area: 212 sq km
+    # Boundaries: {
+    #        'North':'Whitechurch-Stouffville',
+    #        'South': 'Toronto',
+    #        'East': 'Pickering',
+    #        'West': 'Vaughan'
+    #   }
+
+    return {
+        "City Name": cityName,
+        "Province": province,
+        "Region": region,
+        "Location": location,
+        "Area": landArea,
+        "Boundaries": boundaries
+    }
+
+    
