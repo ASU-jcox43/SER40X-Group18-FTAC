bce52b7caae389a50b113a04197ab5bb37e0f473
Brandon Kan <brandon.kwan.kan@gmail.com>
2025-11-07 19:52:11 -0700

    automated profile generation for contact information

--- municipality_profile/profile_manager.py
+++ municipality_profile/profile_manager.py
@@ -2,19 +2,18 @@ from pprint import pprint
 
 
 def createDemographic(
-    population, avgAge, ethnicityCompisition, houseSize, educationLevels
+    population, avgAge, ethnicityComposition, houseSize, educationLevels
 ):
     # Create demographic information
     # Ethnicity composition includes percentage for each ethinicity in the municipality
     # Education levels include percentage of people with certain education levels
     return {
         "Average Age": avgAge,
-        "Ethnicity Compisition": ethnicityCompisition,
+        "Ethnicity Compisition": ethnicityComposition,
         "Population": population,
         "Average House Hold Size": houseSize,
         "Education Levels": educationLevels,
     }
-
 
 def createGeographic(
     city, province, region, popSqMile, areaSqMiles, lat, long, adjMunicipalities
@@ -66,7 +65,6 @@ def createScore():
         "Friendliness index": 0,
     }
 
-
 def scoreBreakdown():
     # Detailed breakdown of scores for each section
     # Scoring rubric topics are proprietary information and cannot be shared
@@ -101,7 +99,6 @@ def createEconomy(income, min_wage, comm_tax_rates):
         "Commercial Tax Rates": comm_tax_rates,
     }
 
-
 def createMunicipalityProfile(
     name,
     fb_type,
@@ -109,7 +106,7 @@ def createMunicipalityProfile(
     province,
     population,
     avgAge,
-    ethnicityCompisition,
+    ethnicityComposition,
     houseSize,
     educationLevel,  # list of education levels
     income,
@@ -122,66 +119,15 @@ def createMunicipalityProfile(
     lat,
     long,
     adjMunicipalities,  # list of neighboring municipalities
+    contacts # list of contact info 
 ):
-    
-    # Example Contact information for Toronto
-    # automate in the future with web scraping if possible or AI 
-    # --- Toronto Food Business Contacts ---
-
-    contacts = [
-        createContactInfo(
-            department="City of Toronto License & Permit Issuing Office",
-            address="East York Civic Centre, 850 Coxwell Ave, Toronto, ON M4C 5R1",
-            fax="n/a",
-            phoneNumber="311 (within Toronto) or 416-392-2489 (outside Toronto)",
-            email="MLSBusinessLicense@toronto.ca",
-            website="https://www.toronto.ca/services-payments/permits-licences-bylaws/food-trucks-food-carts-ice-cream-trucks/food-trucks/",
-            hours="Monday to Friday, 8:30 AM to 4:30 PM (Closed 12:30 p.m. to 1:30 p.m. and on statutory holidays)",
-        ),
-        createContactInfo(
-            department="Road Allowance Permit Office",
-            address="East York Civic Centre, 850 Coxwell Avenue, Toronto, ON M4C 5R1",
-            fax="n/a",
-            phoneNumber="311 (within Toronto) or 416-392-2489 (outside Toronto)",
-            email="MLSRoadAllowance@toronto.ca",
-            website="https://www.toronto.ca/services-payments/permits-licences-bylaws/road-allowance-permits/",
-            hours="Monday to Friday: 8:30 a.m. to 4:00 p.m.",
-        ),
-        createContactInfo(
-            department="Toronto Public Health Food Safety & Inspections",
-            address="277 Victoria Street, Toronto, ON M5B 1W2",
-            fax="n/a",
-            phoneNumber="416-338-7600",
-            email="publichealth@toronto.ca",
-            website="https://www.toronto.ca/community-people/health-wellness-care/health-programs-advice/food-safety/",
-            hours="Monday to Friday: 8:30 a.m. to 4:30 p.m.",
-        ),
-        createContactInfo(
-            department="Green P Parking Permits (Food Truck Parking)",
-            address="33 Queen Street East, Toronto, ON M5C 1R5",
-            fax="n/a",
-            phoneNumber="416-393-7275",
-            email="events@greenpmobility.com",
-            website="https://www.greenp.com/",
-            hours="Monday to Friday: 8:30 a.m. to  4:30 p.m.",
-        ),
-        createContactInfo(
-            department="Technical Standards & Safety Authority (TSSA)",
-            address="345 Carlingview Drive, Toronto, ON M9W 6N9",
-            fax="n/a",
-            phoneNumber="1-877-682-8772",
-            email="customerservices@tssa.org",
-            website="https://www.tssa.org/",
-            hours="Monday to Friday: 8:00 a.m. to 5:00 p.m.",
-        ),
-    ]
 
     profile = {
         "Name": name,
         "fb_type": fb_type,
         "Province": province,
         "Demographic": createDemographic(
-            population, avgAge, ethnicityCompisition, houseSize, educationLevel
+            population, avgAge, ethnicityComposition, houseSize, educationLevel
             ),
         "Economy": createEconomy(income, min_wage, comm_tax_rates),
         "Geographic": createGeographic(
@@ -189,12 +135,7 @@ def createMunicipalityProfile(
             areaSqMiles, lat, long, adjMunicipalities
             ),
         "Contact Information": contacts,
-        "last Updated": last_updated,
+        "last Updated": last_updated
     }
-
-    # Example: pprint all contacts
-    for contact in contacts:
-        print(f"Contact added: {contact['Department']}")
-        # pprint(contact)
 
     return profile
