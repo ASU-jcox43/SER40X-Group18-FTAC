ca46cc42d782460d3ba491f820f183b3b2f1b296
Brandon Kan <brandon.kwan.kan@gmail.com>
2025-11-20 19:48:18 -0700

    ability to access relevant fields from json profiles

--- municipality_profile/search_manager.py
+++ municipality_profile/search_manager.py
@@ -2,17 +2,33 @@ import json
 import pprint
 from pathlib import Path
 
+# Relevant json fields to search through 
+RELEVANT_FIELDS = [
+    "friendlinessScore", "friendlinessScoreBreakdown",
+    "Geographic.City", "Province", "Geographic.Region"
+    ]
+
+def get_nested_data(data, path):
+    keys = path.split(".")
+    for k in keys:
+        if isinstance(data, dict):
+            data = data.get(k)
+        else:
+            return None
+    return data
 
 # Load all profiles
-
 def load_profiles():
     profiles_dir = Path(__file__).parent / "profiles"
     profiles = []
 
-    for file in profiles_dir.glob("*.json"):
+    for file in profiles_dir.glob("*_profile.json"):
         with open(file, "r") as f:
             data = json.load(f)
-            profiles.append(data)
+            # Filter for relevant json fields
+            filtered = {key.replace(".", "_"): 
+                        get_nested_data(data, key) for key in RELEVANT_FIELDS}
+            profiles.append(filtered)
 
     return profiles
 
