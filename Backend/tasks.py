ce32f307dfa45ba81d7aab10998a785c3005f974
Ben Stefl <benstefl8@gmail.com>
2026-01-19 20:55:53 -0700

    Project starts inside docker container, scraper still gives errors

--- Backend/tasks.py
+++ Backend/tasks.py
@@ -1,8 +1,8 @@
 from celery import Celery
 import subprocess
-from Logic.scrapers.document_scraper.spiders.DocumentScraper import *
-from Logic.OCRProcessor.ocr_processor import process_pdfs
-from Logic.extraction.text_extraction import extract
+from .Logic.scrapers.document_scraper.spiders.DocumentScraper import *
+from .Logic.OCRProcessor.ocr_processor import process_pdfs
+from .Logic.extraction.text_extraction import extract
 
 celery_app = Celery(
     "worker",
@@ -13,14 +13,14 @@ celery_app = Celery(
 @celery_app.task
 def run_document_scraper(start_url: str, layers: int=1, get_pdfs: bool=True, rex: str|None=None):
     name_regex = r"(?<=.)\w*(?=\.ca\W)"
-    output_path = f'Service/Links/{re.findall(name_regex, start_url)[0]}.json'
+    output_path = f'scraper_output/{re.findall(name_regex, start_url)[0]}.json'
     command = [
         'scrapy', 'runspider',
         '-a', f'start_url={start_url}',
         '-a', f'layers={layers}',
         '-a', f'get_pdfs={get_pdfs}',
         '-a', f'rex={rex}',
-        'Logic/scrapers/document_scraper/spiders/DocumentScraper.py',
+        'Backend/Logic/scrapers/document_scraper/spiders/DocumentScraper.py',
         '-O', f'{output_path}'
     ]
     subprocess.run(command, text=True)
