9241d62ad452b67968223159eb52709a2c5f64b3
JacobB14210 <128636188+JacobB14210@users.noreply.github.com>
2026-01-29 18:24:44 -0700

    reverted docker mongodb changes made by me

--- Backend/Logic/mongo_db/connection.py
+++ Backend/Logic/mongo_db/connection.py
@@ -1,6 +1,4 @@
 from pymongo import MongoClient
-import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-CLIENT = MongoClient(MONGO_URL)
+CLIENT = MongoClient("mongodb://localhost:27017")
 DB = CLIENT["CapstoneDB"]
