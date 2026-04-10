e61b9bc466dc4bff62081707b7dbdc5390e087f8
Brandon Kan <brandon.kwan.kan@gmail.com>
2026-04-10 15:01:19 -0700

    Refactor DocumentScraperPipeline: Simplify item processing

--- Backend/Logic/scrapers/document_scraper/pipelines.py
+++ Backend/Logic/scrapers/document_scraper/pipelines.py
@@ -9,7 +9,7 @@ from itemadapter import ItemAdapter
 from pymongo import MongoClient
 
 class DocumentScraperPipeline:
-    MONGO_DB = MongoClient("mongodb://ftac-mongo:27017").get_database("CapstoneDB")
+    MONGO_DB = MongoClient("mongodb://ftac-mongo:27017").get_database("CapstoneDB") #not "ftac"
     
     def __init__(self, crawler):
         self.crawler = crawler
@@ -21,16 +21,10 @@ class DocumentScraperPipeline:
     
     def process_item(self, item):
         # db.scrapy_output.updateOne({"_id": self.municipality_name}, {$push: {"links": ItemAdapter(item).asdict()}, $set: {"valid": False}})
-        item_dict = ItemAdapter(item).asdict()
         self.MONGO_DB["scrapy_output"].update_one(
             filter={"_id": self.municipality_name},
             update={
-                "$push": {"urls": {
-                    "name": item_dict['name'],
-                    "number": item_dict['number'],
-                    "year": item_dict['year'],
-                    "url": item_dict['url']
-                    }},
+                "$push": {"urls": ItemAdapter(item).asdict()['url']},
                 "$set": {"valid": False}
             },
             upsert=True
