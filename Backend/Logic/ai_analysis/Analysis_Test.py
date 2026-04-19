cf122dc91a4471f494445e5fcfcc4ec541e87092
ASU-jcox43 <jcox43@asu.edu>
2026-04-19 16:12:28 -0700

    Added comment explanations for Analysis_Test, analyzer_anthropic, and analyzer_openai

--- Backend/Logic/ai_analysis/Analysis_Test.py
+++ Backend/Logic/ai_analysis/Analysis_Test.py
@@ -2,6 +2,9 @@ import unittest
 import rubric_analyzer_anthropic
 import time
 
+# This test just makes sure that this file's AI analysis is done in under two minutes. If over 120 seconds, it fails,
+# otherwise it passes.
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         file_path = "C:/Users/Jacob/repos/SER40X/SER40X-Group18-FTAC/Backend/test_documents/Calgary_Food_Trucks_Copied_And_Pasted.txt"''
--- Backend/Logic/ai_analysis/rubric_analyzer_anthropic.py
+++ Backend/Logic/ai_analysis/rubric_analyzer_anthropic.py
@@ -1,6 +1,15 @@
 import os
 import anthropic
 
+# This is the main work AI analysis part of the project using Anthropic Claude.
+# The idea is similar to the previous OpenAI test analysis part, but this instead imports in the official prompt,
+# which right now is being opened as text. It is read, and then it can be called with download_analysis.
+# Give download analysis the document, and then a True or False if it is a file that needs to be read or just text
+# that is already ready to be analyzed. Give it some time, then it will create a document in the Downloaded_Analyses
+# folder. It uses your Anthropic key as set by your system, so for example, I have my key set in Windows so that it uses
+# it without me having to put anything in the public code. For testing purposes, feel free to paste in your code plainly
+# but just make sure it never sees the public access.
+
 client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 MODEL = "claude-haiku-4-5"
 
